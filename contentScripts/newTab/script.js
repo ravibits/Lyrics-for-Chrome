378b02f5a076720a97f9685d405c44a4e2276257
Harmen <harmenstoppels@gmail.com>
2011-05-25 16:45:23 +0200

    work on the new tab page continues

--- contentScripts/newTab/script.js
+++ contentScripts/newTab/script.js
@@ -19,43 +19,49 @@ LyricsPlugin.prototype.init = function(){
   this.elements.searchForm = $('#lfc-search-form');
   
   // The input field for manual search
-  this.elements.searchInput = $('#lfc-search-lyrics');
+  this.elements.searchInput = $('#lfc-search-input');
   
   // There is no remove button
   this.elements.removeEl = $(null);
   
+  this.elements.changeLyrics = $('#lfc-changeLyrics');
+  
   // Make sure you hide the outer wrapper in first instance
   this.hide();
-  
-  $('#lfc-title').text(this.currentLyrics.title);
   
   // And also hide the lyrics and form div
   this.elements.searchForm.hide();
   this.elements.lyricsContent.hide();
   
+  this.elements.changeLyrics.bind('click', function(e){
+    e.preventDefault();
+    
+    that.showSearchForm();
+  });
+  
   this.elements.searchForm.bind('submit', function(e) {
     e.preventDefault();
     
     // Set the title and try to get the lyrics again
-    that.currentLyrics.title = that.elements.searchInput.val();
+    that.handleNewTitle(that.elements.searchInput.val());
     that.queryLyrics();
-    
-    $('#lfc-title').text(that.currentLyrics.title);
   });
 };
 
-LyricsPlugin.prototype.handleTitleFromBackground = function(title){
+LyricsPlugin.prototype.handleNewTitle = function(title){
   this.currentLyrics.title = title;
   document.title = [title, ' \u2014 ', chrome.i18n.getMessage('extName')].join('');
+  
+  $('#lfc-title').text(this.currentLyrics.title);
 };
 
 var LP = new LyricsPlugin();
+LP.init();
 
 chrome.extension.onRequest.addListener(function(request, sender, callback) {
   switch(request.action){
     case 'initNewTab':
-      LP.handleTitleFromBackground(request.title);
-      LP.init();
+      LP.handleNewTitle(request.title);
       LP.onPageActionClicked();
     break;
     case 'showLyricsOnPage':
